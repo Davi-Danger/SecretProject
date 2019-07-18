169509027f6b235c68bfa082c80b7d202a070201
Davi-Danger <daveysdownfall@gmail.com>
2019-07-18 16:23:13 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const start =
-    () => {
-      setInterval(100, vibrate());
-    }
-
 const vibrate = () => {
   window.navigator.vibrate(100);
   console.log(vibrate);
 };
+
+const start = () => {
+  setInterval(100, vibrate());
+}
